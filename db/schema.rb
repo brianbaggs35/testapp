b954f5288a41584471c50d7dc9ca9c40184e57db
Brian Baggs <brianbaggs@hotmail.com>
2024-08-10 11:53:07 -0300

    dump schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_113830) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_01_113830) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,5 +18,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_113830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
