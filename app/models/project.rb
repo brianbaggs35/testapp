251fe6bc8a598460d05693894a9fcd6c9baff68c
Brian Baggs <brianbaggs@hotmail.com>
2025-03-23 12:46:36 -0300

    Major update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
 class Project < ApplicationRecord
-    scope :almost_completed, -> { where('percent_complete > 75.0') }
+    has_many :tasks
+    after_initialize :set_defaults
+
+    validates_presence_of :title, :description, :percent_complete
+
+    scope :almost_complete, -> { where('percent_complete > 85.0') }
+    scope :still_needs_work, -> { where('percent_complete < 25.0') }
+
+    def set_defaults
+        self.percent_complete ||= 0.0
+    end    
 end
